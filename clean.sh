37629cc386926768d5a549c6d9cf3f081cebb305
Luis Toledo <luis@variacode.com>
2018-03-08 17:11:32 -0300

    First Commit

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+# clean up docker env
+docker-compose down --volumes --remove-orphans
---
+++ rundeck/scripts/config.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#set uuid
+UUID=$(uuidgen)
+cat >>$RDECK_BASE/etc/framework.properties <<END
+rundeck.server.uuid = $UUID
+END
+
+API_KEY=${API_KEY:-letmein99}
+cat > $RDECK_BASE/etc/tokens.properties <<END
+admin: $API_KEY
+END
+
+#rd settings
+mkdir ~/.rd
+cat > ~/.rd/rd.conf <<END
+export RD_TOKEN=$API_KEY
+export RD_URL="http://localhost:8080/rundeckpro"
+export RD_COLOR=0
+export RD_OPTS="-Dfile.encoding=utf-8"
+export RD_HTTP_TIMEOUT=300
+#export RD_DEBUG=1
+END
+
+# pass rundeck home to tomcat
+
+cat > /usr/local/tomcat/bin/setenv.sh <<END
+JAVA_OPTS="$JAVA_OPTS -Drdeck.base=$RDECK_BASE -Drundeck.config.location=$RDECK_BASE/etc/rundeck-config.properties"
+END
+
+# undeplpy rundeckpro and load log4j.properties
+cd $CATALINA_HOME/webapps
+unzip -d rundeckpro rundeckpro.war
+
+cp $RDECK_BASE/etc/log4j.properties $CATALINA_HOME/webapps/rundeckpro/WEB-INF/classes/
+
